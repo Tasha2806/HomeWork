b3efd2c6e618806192461347ba72d34f98a9a47a
Tashagit <natalya_koncevaya@mail.ru>
2023-10-01 16:22:57 +0300

    задача 14

--- main.py
+++ main.py
@@ -23,20 +23,22 @@
 # Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
 # Помогите Кате отгадать задуманные Петей числа.
 
-s = int(input('sum='))
-p = int(input('prod='))
-for x in range(s):
-    for y in range(s):
-        if x + y == s and x * y == p:
-            print(x,y)
-
-
-
-
-
+# s = int(input('sum='))
+# p = int(input('prod='))
+# for x in range(s):
+#     for y in range(s):
+#         if x + y == s and x * y == p:
+#             print(x,y)
 
 
 
 
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k),
 # не превосходящие числа N.
+
+
+n = int(input('n='))
+x = 0
+while 2 ** x <= n:
+    print(2 ** x)
+    x += 1
